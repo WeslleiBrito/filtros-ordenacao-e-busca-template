dd2345ad31ee277d445e0fd60d6712f5fc43fb9e
WeslleiBrito <wesllei.wbs@gmail.com>
2023-03-06 21:50:56 -0300

    atividade finalizada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import pokemons from "./pokemon/pokemon.json";
 import PokemonCard from "./components/PokemonCard/PokemonCard";
 import { getColors } from "./utils/ReturnCardColor";
 import Header from "./components/Header/Header.js";
+import { useState } from "react";
 const GlobalStyle = createGlobalStyle`
   *{
     padding: 0;
@@ -18,12 +19,57 @@ const CardsContainer = styled.div`
   justify-items: center;
 `;
 function App() {
+
+  const [findId, setFindId] = useState('')
+  const [findName, setFindName] = useState('')
+  const [order, setOrder] = useState('')
+
+  const handleInputFindChange = (event) => {
+    setFindId(event.target.value)
+  }
+
+  const handleInputFindName = (event) => {
+    setFindName(event.target.value)
+  }
+
+  const handleOrder = (event) => {
+    setOrder(event.target.value)
+  }
+
+  console.log(order)
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header 
+        valueId={findId} 
+        handleInputFindChange={handleInputFindChange}
+        valueName={findName}
+        handleInputFindName={handleInputFindName}
+        handleOrder={handleOrder}
+      />
       <CardsContainer>
-        {pokemons.map((pokemon) => {
+        {pokemons.filter((item) => {
+          if(findId){
+            return item.id.includes(findId)
+          }else{
+            return item
+          }
+        }).filter((item) => {
+          if(findName){
+            return item.name.english.toLocaleLowerCase().includes(findName.toLocaleLowerCase())
+          }else{
+            return item
+          }
+        }).sort((a, b) => {
+          if(order === "crescent"){
+            return a.id - b.id
+          }
+
+          if(order === "decrescent"){
+            return b.id - a.id
+          }
+        })
+        .map((pokemon) => {
           return <PokemonCard
           cardColor={getColors(pokemon.type[0])}
           key={pokemon.id}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Header = () => {
+const Header = ({valueId, handleInputFindChange, valueName, handleInputFindName, handleOrder}) => {
   const pokemontypesArray = [
     "Normal",
     "Fire",
@@ -25,12 +25,12 @@ const Header = () => {
 
   return (
     <Container>
-      <input type="number" placeholder="Buscar por id" />
-      <input type="text" placeholder="Buscar por nome" />
-      <select>
+      <input type="number" value={valueId} onChange={handleInputFindChange} placeholder="Buscar por id" />
+      <input type="text" value={valueName} onChange={handleInputFindName} placeholder="Buscar por nome" />
+      <select onChange={handleOrder}>
         <option value="">Ordenar</option>
-        <option value="">Crescente</option>
-        <option value="">Decrescente</option>
+        <option value="crescent">Crescente</option>
+        <option value="decrescent">Decrescente</option>
       </select>
       <select name="tipo" id="tipo">
         <option value="">Selecione um tipo</option>
